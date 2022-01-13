d24143db546f917de0b48259583500ffdcf49dad
Wasef-Hussain <12345678wasef@gmail.com>
2022-01-13 20:40:21 +0500

    first commit

---
+++ src/styles/App.style.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+
+
+
+
+
+
+
+export const Wrapper = styled.div `
+font-family: Arial,sans-seriff;
+display:flex;
+align-items:center;
+justify-content:center;
+flex-direction:column;
+height:100vh;
+
+
+div{
+    font-size:3rem;
+    background:lightblue;
+    padding:20px 40px;
+    border-radius:40px;
+    margin-top:20px;
+}
+`;
